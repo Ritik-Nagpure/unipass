1588d06b9ef53f2ffad3c291742f3fd422432618
Ritik <ritiknagpure@gmail.com>
2026-08-31 00:28:23 +0530

    deploy fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,18 +3,23 @@ import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
 
+const port = process.env.BACKEND_PORT || 3001;
 const app = express();
 
 app.use(cors({ origin:"http://localhost:3000"}));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets', 'index.html'));
+});
 
 app.get('/api', (req, res) => {
   const time = new Date().toLocaleTimeString()
   res.send({ message: `The Api is running fine response sent at ${time}` });
 });
 
-const port = process.env.BACKEND_PORT || 3001;
+
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
